0e0e85cb661f390920fd32b6c69e4d6146735527
Jan0660 <jan0660@tutanota.com>
2021-02-21 14:40:41 +0100

    Returns 404 when redirect is not found

--- Controllers/RedirectController.cs
+++ Controllers/RedirectController.cs
@@ -20,44 +20,61 @@ namespace RedirectPage.Controllers
 </body>
 </html>";
         [Route("/r/{name}")]
-        public ContentResult Index(string name)
+        public IActionResult Index(string name)
         {
-            var redir = Redirects.Dict[name];
-            if (Request.Headers["User-Agent"].ToString().Contains("https://discordapp.com"))
+            RedirectInfo redir;
+            try
+            {
+                redir = Redirects.Dict[name];
+            }
+            catch
+            {
+                return new ContentResult
+                {
+                    ContentType = "text/html",
+                    StatusCode = IsNotHuman() ? 200 : 404,
+                    Content = BoilerPlateStart + GetRedirectMetadataHtml(new RedirectInfo()
+                    {
+                        Title = "Redirect link not found",
+                        EmbedColor = "ff0000",
+                        SiteName = "Redirif"
+                    })
+                    + @"</head><body>Redirect link not found</body></html>"
+                };
+            }
+
+            if (IsNotHuman())
             {
                 Console.WriteLine("based discord");
                 return new ContentResult
                 {
                     ContentType = "text/html",
                     StatusCode = (int)HttpStatusCode.OK,
-                    Content = boil($@"<meta property=""og:title"" content=""{redir.Title.SanitizeHtml()}"" />
-<meta property=""og:description"" content=""{redir.Description.SanitizeHtml()}"" />
-<meta property=""og:site_name"" content=""{redir.SiteName.SanitizeHtml()}"" />
-<meta name=""twitter:image"" content=""{redir.ImageUrl.SanitizeHtml()}"" />
-<meta name=""theme-color"" content=""#{redir.EmbedColor.SanitizeHtml()}"" />
-<meta data-react-helmet=""true"" name=""msapplication-TileColor"" content=""#{redir.EmbedColor.SanitizeHtml()}"">
-<meta data-react-helmet=""true"" name=""theme-color"" content=""#{redir.EmbedColor.SanitizeHtml()}"">" +
-                                                                  (redir.SmallImage ? "" : @$"<meta name=""twitter:card"" content=""summary_large_image"">"))
+                    Content = boil(GetRedirectMetadataHtml(redir))
                 };
             }
             Response.Redirect(redir.Url);
             Console.WriteLine("based human");
             return Content("poggers");
         }
-        [Route("/elpoggers/champ")]
-        public string boil(string str)
+        private string boil(string str)
         {
             return BoilerPlateStart + str + BoilerPlateEnd;
         }
-        [Route("/poggers/champ")]
-        public ContentResult bruh(string str)
+
+        private string GetRedirectMetadataHtml(RedirectInfo redir)
         {
-            return new ContentResult
-            {
-                ContentType = "text/html",
-                StatusCode = (int)HttpStatusCode.OK,
-                Content = boil("<meta property=\"og:title\" content=\"bruh\" />")
-            };
+            return $@"<meta property=""og:title"" content=""{redir.Title.SanitizeHtml()}"" />
+<meta property=""og:description"" content=""{redir.Description.SanitizeHtml()}"" />
+<meta property=""og:site_name"" content=""{redir.SiteName.SanitizeHtml()}"" />
+<meta name=""twitter:image"" content=""{redir.ImageUrl.SanitizeHtml()}"" />
+<meta name=""theme-color"" content=""#{redir.EmbedColor.SanitizeHtml()}"" />
+<meta data-react-helmet=""true"" name=""msapplication-TileColor"" content=""#{redir.EmbedColor.SanitizeHtml()}"">
+<meta data-react-helmet=""true"" name=""theme-color"" content=""#{redir.EmbedColor.SanitizeHtml()}"">" +
+                        (redir.SmallImage ? "" : @$"<meta name=""twitter:card"" content=""summary_large_image"">");
         }
+
+        private bool IsNotHuman()
+            => Request.Headers["User-Agent"].ToString().Contains("https://discordapp.com");
     }
 }
